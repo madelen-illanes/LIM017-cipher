7f31c64ca05be08b2686b447ff03daf270ea4dc8
madelen <madelen2811@gmail.com>
2022-02-22 14:28:38 -0500

    PRIMER CIPHER

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,42 @@
 const cipher = {
-  // ...
-};
+  encode: (offset , string ) => { 
+  let result = ""
+  for (let i=0; i<string.length;i++){
+      let possAscii= string.charCodeAt(i);
+      let possOffset= ( possAscii - 65 + Number(offset)) %26 + 65;
+      let letter = String.fromCharCode(possOffset);
+      result = result + letter;
+    }
+   
+    if (!string) throw new TypeError("you did not enter any message");
+    if (typeof string!== "string") throw new TypeError ("the value entered is not text");
+     
+    return result
+  }, 
 
-export default cipher;
+  decode: (offset , string ) => { 
+   
+  let result = ""
+  for (let i=0; i<string.length;i++){
+      let possAscii= string.charCodeAt(i);
+      let possOffset= (possAscii + 65 - Number(offset)) %26 + 65;
+      let letter = String.fromCharCode(possOffset);
+      result = result + letter;
+    }
+    if (!string) throw new TypeError("you did not enter any message");
+    if (typeof string !== "string") throw new TypeError ("the value entered is not text");
+    return result
+  }, 
+
+  }
+
+  //console.log(cipher.encode(2, "MADELEN"))
+ //console.log(cipher.decode( 2, "O"))
+  export default cipher;
+  
+
+ 
+
+
+
+
